38bac0287c9cfa3a0c73801336083ee0da6e5d35
Sparsh <sparshkumar14998@gmail.com>
2021-03-13 16:27:18 +0530

    done modification in get events controller

--- controllers/getevents.js
+++ controllers/getevents.js
@@ -8,19 +8,26 @@ const getevents = (req, res) => {
 
     try {
 
-        const { startDate, endDate } = _.pick (req.body, ['startDate', 'endDate']);
+        let { startDate, endDate } = _.pick (req.body, ['startDate', 'endDate']);
         // TODO - We can validate if the values entered startDate and endDate must be of Number type
         // startDate and endDate should be in milliseconds
+
         if (!startDate || !endDate) {
             throw new Error ('please enter both the start date and end date');
         }
 
+        startDate = parseInt(startDate);
+        endDate = parseInt (endDate);
+
         Event.find ({
+            
             datetime: {
-                $in: {
-                    $range: [startDate, endDate + 1] // it will find events whose datetime is in range b/w startDate and endDate
-                }
+                $and: [
+                    {$gte: startDate},
+                    {$lte: endDate}
+                ]
             }
+            
         }).then ((events) => {
 
             return res.status (200).send ({
